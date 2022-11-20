7036924f584f69088be37cb10b3722a94d5e98e5
huyngo512 <hngo419512@gmail.com>
2022-11-20 13:11:20 +0700

    add haha

---
+++ components/Hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Component } from "react";
+import { Text } from "react-native";
+export default class Hello extends Component {
+    render(){
+        return (
+            <Text> Hello</Text>
+        );
+    }
+}
